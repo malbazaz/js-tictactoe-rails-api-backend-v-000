ca26069f95d5aab017b53af1137c4b496ae83c8a
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-05 18:15:55 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  resources :games
+  resources :games, only: [:index, :show, :create, :update]
   # post '/games', to: 'games#create'
   # get '/games/:id', to: 'games#show'
   # patch '/games/:id', to: 'games#update'

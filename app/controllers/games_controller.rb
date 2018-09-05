ca5a5a7f2c59f1cff75610c87ddebdc6e08fe30f
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-05 18:03:30 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,18 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
 
 end
